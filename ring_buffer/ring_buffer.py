6eadc97bab57661dfd7191990094060ce9e2eb84
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-28 18:14:57 +0100

    final solution to ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -11,36 +11,49 @@ class RingBuffer:
     def append(self, item):
       #if there's still capacity for storage, append new item to head
       if self.storage.length < self.capacity:
-          self.storage.add_to_head(item)
-          self.current = item # the most recently appended item
-      #if the current is up to the capacity
-      if self.storage.length == self.capacity:
-      # update current value to the new item
-          self.current.value = item
-      # if we're currently at the tail, and the most recent item is the head
-      # we need to overwrite the oldest item
-          if self.current is self.storage.tail:
-          # go to head 
-            self.storage.remove_from_tail()
-            self.storage.add_to_tail(item)
-            self.current = item
-          else:
-          # else, go to the next
-            self.current = self.current.next
-          
-      
+          self.storage.add_to_tail(item)
+          self.current = self.storage.head
+      #else, if the current is equal or more than the capacity
+      else:
+      # we need to overwrite the oldest node, remove from the head and add item to the tail
+          overwritten_node = self.storage.head
+          self.storage.remove_from_head()
+          self.storage.add_to_tail(item)
+          #if the overwritten node is the current node, move it to the tail
+          if overwritten_node == self.current:
+              self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
-        current_item = self.storage.tail
-        # whilst item is not None, append the value to list
-        while current_item is not None:
-           list_buffer_contents.append(current_item.value)
-           # Move current_item temporary variable onto next item
-           current_item = current_item.prev
+        # # TODO: Your code here
+        list_buffer_contents.append(self.current.value)
+
+        if self.current == self.storage.tail:
+            node = self.storage.head
+        else:
+            node = self.current.next
+
+        while node is not self.current:
+            list_buffer_contents.append(node.value)
+            node = node.next if node.next else self.storage.head
+
+        return list_buffer_contents
+
+        # return list_buffer_contents
+        # list_buffer_contents = []
+
+        # list_buffer_contents.append(self.current.value)
+
+        # if self.current == self.storage.tail:
+        #     node = self.storage.head
+        # else:
+        #     node = self.current.next
+
+        # while node is not self.current:
+        #     list_buffer_contents.append(node.value)
+        #     node = node.next if node.next else self.storage.head
 
         # return list_buffer_contents
 
